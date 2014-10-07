a500d35bcd541d71d6d393c83ca2d29239bf00ef
michubest <michubest@gmail.com>
2014-10-07 17:37:34 +0200

    Create Zadanie1.php

---
+++ Zadanie1.php
@@ -0,0 +1,24 @@
+//Król tuskus wprowadzil nowe podatki.Nigdy nie lubił matematyki , więc ustalił najwyższe podatki dla jej nauczycieli -
+//az 57% ,dla nauczycieli  biologii 8% , a dla nauczycieli polskiego - 23%.Król Tuskus zabierze podatek z kont nauczycieli.
+//Ciebie , jako najlepszego programiste w Cebulandii , poprosił o stworzenie tabelki w której napiszesz imie, nazwisko ,
+//oraz stan konta po odprowadzeniu podatku .Król Tuskus nie będzie tolerował nieposłuszeństwa.
+
+$konta = array(
+
+"1"=array(
+  imie=>'Wioletta'
+  nazwisko=>'Marcinek'
+  stan_konta=>6792zl
+  przedmiot=>'Polski'),
+"2"=array(
+  imie=>'Tomasz'
+  nazwisko=>'Orka'
+  stan_konta =>
+  przedmiot=>'Biologia'
+  ),
+"3"=array(
+  imie=>'Marta'
+  nazwisko=>'Stopa'
+  stan_konta=>3151zl
+  przedmiot=>'Matematyka'),
+  )
