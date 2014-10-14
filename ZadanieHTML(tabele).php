d695352da129c81452a2e8add755c31adaffa2bf
michubest <michubest@gmail.com>
2014-10-14 16:09:00 +0200

    Create ZadanieHTML(tabele).php

---
+++ ZadanieHTML(tabele).php
@@ -0,0 +1,3 @@
+//http://i57.tinypic.com/6xvr6f.jpg 
+//W tym linku znajdziecie tabele winikow finalu ESM virtus.pro VS Ninja In Pijamas . Zrobcie 2 tabelki , ktore wyswietla 
+//Statystyki graczy z druzyn NiP i VP . 
